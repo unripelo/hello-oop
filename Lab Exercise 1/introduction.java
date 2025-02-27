c324dd9814e7dfcfd84c7c35d28c5ee9f8218d8f
Ira Narisma <narisma.ira.chloie@gmail.com>
2025-02-27 21:30:13 +0800

    adding first file

---
+++ Lab Exercise 1/introduction.java
@@ -0,0 +1,14 @@
+class introduction{
+public static void main(String args[ ]) 
+    {
+        String name = "Ira Chloie C. Narisma";
+        int age = 20;
+        String hobbies = "Music, Photography, Watching animations and dramas";
+        String influence = "Ada Lovelace";
+
+        System.out.println("Name: " + name);
+        System.out.println("Age: " + age);
+        System.out.println("Hobbies: " + hobbies);
+        System.out.println("Who influence you to take BSCS? " + influence);
+    }
+}
