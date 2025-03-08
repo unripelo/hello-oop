f4440e8ff5c2f6bfe5c5af2b163c1bcd5612e80a
Ira Narisma <narisma.ira.chloie@gmail.com>
2025-03-08 21:13:45 +0800

    lab 2 edit

--- Lab Exercise 2/Act2Ave.java
+++ Lab Exercise 2/Act2Ave.java
@@ -2,20 +2,22 @@ import java.util.Scanner;
 
 class Act2Ave {
   public static void main(String[] args) {
-    Scanner sc = new Scanner(System.in);
+    Scanner scanner = new Scanner(System.in);
 
     System.out.println("Enter name, age and salary:");
 
     // String input
-    String name = sc.nextLine();
+    String name = scanner.nextLine();
 
     // Numerical input
-    int age = sc.nextInt();
-    double salary = sc.nextDouble();
+    int age = scanner.nextInt();
+    double salary = scanner.nextDouble();
 
     // Output input by user
     System.out.println("Name: " + name);
     System.out.println("Age: " + age);
     System.out.println("Salary: " + salary);
+
+    scanner.close();
   }
 }
